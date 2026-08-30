7db012b55e43ad037ad72fc90616108d1e95c3da
yubeen0412 <lyubeen07@gmail.com>
2026-08-30 21:56:53 +0900

    pracrice 5

--- main.c
+++ main.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
-#include <string.h>
 
-void main(void) {
-	char str[30]="Happy C programming";
-	printf("length of string /%s/ : %i", str, strlen(str));
+int main(void)
+{
+    FILE *fp;
+    char word1[100];
+    char word2[100];
+    char word3[100];
+
+    fp = fopen("sample.txt", "w");
+
+    if (fp == NULL)
+    {
+        printf("파일을 못열음\n");
+        return 1;
+    }
+
+    scanf("%s", word1);
+    scanf("%s", word2);
+    scanf("%s", word3);
+
+    fprintf(fp, "%s\n", word1);
+    fprintf(fp, "%s\n", word2);
+    fprintf(fp, "%s\n", word3);
+
+    fclose(fp);
+
+    return 0;
 }
