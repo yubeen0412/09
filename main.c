6e6ff4d05658078be1d6ff74a94555f335cfdbd6
yubeen0412 <lyubeen07@gmail.com>
2026-08-30 21:22:59 +0900

    practice 4

--- main.c
+++ main.c
@@ -2,10 +2,6 @@
 #include <string.h>
 
 void main(void) {
-	char scr[]="The worst things to eat before you sleep";
-	char dst[100];
-	
-	strcpy(dst, scr);
-	
-	printf("copied string : %s\n", dst);
+	char str[30]="Happy C programming";
+	printf("length of string /%s/ : %i", str, strlen(str));
 }
